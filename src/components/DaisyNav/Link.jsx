1f9049cdf0cb12fb5128f289d38329eb061e2379
Smart-imran <rst.imran4@gmail.com>
2024-09-25 01:07:14 +0600

    adding

--- src/components/DaisyNav/Link.jsx
+++ src/components/DaisyNav/Link.jsx
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 
 const Link = ({route}) => {
     return (
-        <li className="mr-80" key={route.id}>
+        <li className="mr-28 px-6 hover:bg-violet-400" key={route.id}>
             <a href={`route.path`}> {route.name}</a>
 
         </li>)
     
 };
-//proptypes
+//prototypes
 
 Link.propTypes = {
     route: PropTypes.object
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,17 +19,20 @@ const NavBar = () => {
 
 
     return (
-        <nav>
+        <nav className="text-black bg-yellow-200 p-8">
             <div className="md:hidden text-2xl" onClick={()=>setOpen(!open)}>
 
                 {
                     open === true ? <AiOutlineClose/> : <IoMenu/>
                    
                 }
-
                
             </div>
-            <ul className="md:flex absolute bg-yellow-200">
+            <ul className={`md:flex duration-1000 absolute md:static
+                
+                ${open ? 'top-16' : '-top-60'}
+
+                bg-yellow-200 px-8`}>
                 {
                     routes.map(route => <Link key={route.id} route={route}></Link>)
                 }
